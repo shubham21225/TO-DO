849f6723f671b1c2b354740a29bfb6d4c3717b1f
shubham gupta <154531761+shubham21225@users.noreply.github.com>
2024-09-05 18:51:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let inputbox= document.querySelector("#input-box");
+// let = document.querySelector(" ");
+// let = document.querySelector(" ");
+
+let listcontainer = document.querySelector("#list-container")
+
+
+
+function addTask(){
+    if(inputbox.value === ""){
+        alert("you must write something")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+    inputbox.value = "";
+    saveData();
+}
+
+listcontainer.addEventListener("click",function(e){
+            if(e.target.tagName ==="LI"){
+                e.target.classList.toggle("checked");
+                saveData();
+            }
+            else if(e.target.tagName === "SPAN"){
+                e.target.parentElement.remove();
+                saveData();
+            }
+},false);
+
+
+function saveData(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showlist(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+
+showlist();
